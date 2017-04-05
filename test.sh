6e1a73115b011c9cf4d79c469533f3e5674c71c0
DKlesel <dck35@txstate.edu>
2017-04-05 17:54:57 -0500

    Calum's upload of in-class assignment

---
+++ test.sh
@@ -0,0 +1,8 @@
+echo "Testing hwInput_GP.txt"
+python helloworld.py < hwInput_GP.txt
+echo "Testing hwInput_GP_quoted.txt"
+python helloworld.py < hwInput_GP_quoted.txt
+echo "Testing hwInput_2.txt"
+python helloworld.py < hwInput_2.txt
+echo "Testing hwInput_2_quoted"
+python helloworld.py < hwInput_2_quoted.txt
