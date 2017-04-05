54af6e7ebee8431775690198f4e8628e02bb52ac
CodySpradling <c_s255@txstate.edu>
2017-04-05 18:02:25 -0500

    CLS testing travis_ci

---
+++ test.sh
@@ -0,0 +1,5 @@
+python helloworld.py < hwInput_4.txt
+python helloworld.py < hwInput_G.txt
+python helloworld.py < hwInput_blah.txt
+python helloworld.py < hwInput_9.txt
+python helloworld.py < hwInput_bl9ah.txt
