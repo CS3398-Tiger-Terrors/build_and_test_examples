6962cab9aee27f9af75148946ffcaf2b07732980
Leo <lss70@txstate.edu>
2017-04-05 17:48:28 -0500

    In Class Assignment 4.5.17

---
+++ test.sh
@@ -0,0 +1,10 @@
+echo "testing hwInput_GP"
+python helloworld.py < hwInput_GP.txt
+
+echo "testing hwInput_GP_quoted"
+python helloworld.py < hwInput_GP_quoted.txt
+echo "testing hwInput_2"
+
+python helloworld.py < hwInput_2.txt
+echo "testing hwInput_2_quoted"
+python helloworld.py < hwInput_2_quoted.txt
