702d2a8d860053114c9f9df869263e32ac994c8d
ntw8 <ntw8@txstate.edu>
2017-04-05 18:29:50 -0500

    Nick's Tests

--- test.sh
+++ test.sh
@@ -8,3 +8,5 @@ echo "Testing hwInput_2_quoted"
 python helloworld.py < hwInput_2_quoted.txt
 echo "Testing hwInput_Cody"
 python helloworld.py < hwInput_Cody.txt
+echo "Testing hwInput_Nick"
+python helloworld.py < hwInput_Nick.txt
